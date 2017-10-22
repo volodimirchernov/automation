2d3f70a776cfa26af6b1f44a32813e442fbf006e
Vladimir <volodimirchernov@gmail.com>
2017-10-22 17:37:28 +0300

    Just try and try start firefox again

--- indian_pytest/Execute/Other/Test_Firefox.py
+++ indian_pytest/Execute/Other/Test_Firefox.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-browser = webdriver.Firefox("../Environment/Drivers/geckodriver.exe")
+firefox_capabilities = DesiredCapabilities.FIREFOX
+firefox_capabilities['marionette'] = True
+firefox_capabilities['binary'] = 'C:\\Users\\vchernov\\Desktop\\drivers\\geckodriver.exe'
+browser = webdriver.Firefox(capabilities=firefox_capabilities)
 browser.get('https://www.google.com')
