30b219e2262f93a97c6a6d38907ca87d25c19cb0
Vladimir <volodimirchernov@gmail.com>
2017-10-30 10:08:40 +0200

    Cross browser testing it's a tuff things

--- indian_pytest/Execute/Chrome_health_status.py
+++ indian_pytest/Execute/Chrome_health_status.py
@@ -1,6 +1,6 @@
 from selenium.webdriver import Chrome
 import pytest
-
+from selenium.webdriver.chrome.options import Options
 from Base_methods.Export_methods import *
 from Base_methods.Mydocs_methods import *
 from Base_methods.Home_methods import *
@@ -10,10 +10,20 @@ from Base_methods.Home_methods import *
 
 @pytest.fixture()
 def chrome_driver(request):
-    driver = Chrome("../Environment/Drivers/chromebrowser.exe")
+
+    # For maximaze (start)
+
+    options = Options()
+    options.add_argument("--start-maximized")
+    driver = Chrome("../Environment/Drivers/chromedriver.exe", chrome_options=options)
+
+    # For maximaze (end)
+
+    # Or driver.set_window_size(1200, 800)
+
     driver.implicitly_wait(20)
     driver.set_page_load_timeout(20)
-    driver.maximize_window()
+    #driver.maximize_window()
     request.addfinalizer(driver.quit)
     return driver
 
